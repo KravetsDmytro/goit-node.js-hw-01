2c63edfa449703bc57ebf8fa6a6367cbd5cf96c5
Kravets_Dmytro <ditur@ukr.net>
2023-08-01 15:24:55 +0300

    option

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ program
   .option("-i, --id <type>", "user id")
   .option("-n, --name <type>", "user name")
   .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
 
 program.parse();
 
